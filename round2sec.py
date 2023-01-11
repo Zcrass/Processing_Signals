2404e34125747161ba80c0070f6b106ce570320f
sgd <guzmandsalvador@gmail.com>
2023-01-10 20:46:11 -0600

    added json file and non-historic mode

--- round2sec.py
+++ round2sec.py
@@ -3,11 +3,12 @@
 import argparse
 from dask import dataframe as dd
 from datetime import datetime, timedelta
+import json
 import logging as lg
 import math
 import sys
 
-from signal_functions import subranges, signal_df
+from signal_functions import check_history, process_signal
 
 if __name__ == '__main__':
     ### define logger
@@ -19,60 +20,99 @@ if __name__ == '__main__':
     stdout_handler.setLevel(lg.INFO)
     logger.addHandler(stdout_handler)
     
-    ### define arguments 
+    # ### define arguments 
     parser = argparse.ArgumentParser(prog = 'Round2Sec', description = 'Round to a resolution of seconds using mean value from data')
-    parser.add_argument('-i', '--input') ### input dir in parquet format
-    parser.add_argument('-s', '--start_date') ### start date to process in format
-    parser.add_argument('-e', '--end_date') ### end date to process
-    parser.add_argument('-v', '--var_cols') ### column names to process
-    parser.add_argument('-b', '--batch_size') ### batch size to process
-    parser.add_argument('-o', '--out_path') ### batch size to process
+    parser.add_argument('-i', '--input') ### input json file
+    # parser.add_argument('-s', '--start_date') ### start date to process in format
+    # parser.add_argument('-e', '--end_date') ### end date to process
+    # parser.add_argument('-v', '--var_cols') ### column names to process
+    # parser.add_argument('-b', '--batch_size') ### batch size to process
+    # parser.add_argument('-o', '--out_path') ### batch size to process
     
-    args = parser.parse_args()
-    
-    
+    # args = parser.parse_args()
+    args = json.load(open(parser.parse_args().input))
     
     ########################
     ### define date format
-    start_date = datetime.strptime(args.start_date, '%Y%m%d')
-    end_date = datetime.strptime(args.end_date, '%Y%m%d') 
-    ### Historico
-    if args.var_cols == 'all':
-        ### load data with all columns
-        cols = list(dd.read_parquet(args.input).columns)
-    else:
-        #### load data with selected columns
-        cols = ['Time'] + args.var_cols
+    start_date = datetime.strptime(str(args['start_date']), '%Y%m%d')
+    # end_date = datetime.strptime(str(args['end_date']), '%Y%m%d') 
+        
+            # try:
+            #     dd.read_parquet(args['out_path', columns='Time'])
+            ### compare star dates to check if historical is correct
+            #     print('compare start dates')
+            #     print(stored_dates)
+    cols = args['var_cols'] 
     logger.info(f'Loading dataframe with {len(cols)} columns...')    
     
-    ### define subranges according to batch size
-    num_sub_ranges = math.ceil((len(cols)-1)/int(args.batch_size))
-    logger.info(f'Processing {num_sub_ranges} batches')
-    sub_starts, sub_ends = subranges(cols, num_sub_ranges, int(args.batch_size))
-
     ### defining naming function
     def name_function(part_ind):
             return f'{day}_{str(part_ind).zfill(6)}.parquet'
         
-    ### run each batch    
-    c = 0  
-    for batch in (range(0, num_sub_ranges)):
-        logger.info(f'Loading batch number: {batch+1}')
-        ### define subrange of columns
-        sub_cols = ['Time'] + cols[sub_starts[batch]:sub_ends[batch]]
-        ### read parquet per batch
-        data = dd.read_parquet(args.input, columns=sub_cols)   
-        ### filter by date and roun to seconds
-        data = data.loc[(data['Time'] >= start_date) & (data['Time'] < (end_date + timedelta(days=1)))]
-        data['Time'] = data['Time'].dt.floor('s')
-        ### compute means
-        data = data.groupby(data['Time']).mean().reset_index()
-        ### compute results table for each column
-        for col in sub_cols[1:]:
-            c += 1
-            table = signal_df(data, col)
-            logger.info(f'Saving signal {c}/{len(cols)}: {col}')
+    if check_history(args['out_path']):
+        logger.warning(f'NO historical data found!')
+        logger.info(f'Running in historical mode')
+        # if args['var_cols'] == 'all':
+        #     ### load data with all columns
+        #     cols = list(dd.read_parquet(args['input']).columns)
+        # else:
+        #     #### load data with selected columns
+        #     cols = ['Time'] + args['var_cols'] 
+        ### define subranges according to batch size
+        # num_sub_ranges = math.ceil((len(cols)-1)/int(args['batch_size']))
+        # logger.info(f'Processing {num_sub_ranges} batches')
+        # sub_starts, sub_ends = subranges(cols, num_sub_ranges, int(args['batch_size']))
+
+
+            
+        for c, col in enumerate(cols):
+            start_date = start_date - timedelta(days=1)
+            data = process_signal(col, start_date, args['input'])
+            logger.info(f'Saving signal {c+1}/{len(cols)}: {col}')
             ### save by date
-            for day in table.Date.unique():
+            for day in data.Date.unique():
                 ### save parquet files
-                table.loc[table.Date == day].to_parquet(path=args.out_path, partition_on=['Date', 'Signal'], name_function=name_function)        
+                data.loc[data.Date == day].to_parquet(path=args['out_path'], partition_on=['Date', 'Signal'], name_function=name_function)        
+
+        # ### run each batch    
+        # c = 0  
+        # for batch in range(0, num_sub_ranges):
+        #     logger.info(f'Loading batch number: {batch+1}')
+        #     ### define subrange of columns
+        #     sub_cols = ['Time'] + cols[sub_starts[batch]:sub_ends[batch]]
+        #     ### read parquet per batch
+        #     data = dd.read_parquet(args['input'], columns=sub_cols)   
+        #     ### filter by date and roun to seconds
+        #     data = data.loc[(data['Time'] >= start_date)]
+        #     data['Time'] = data['Time'].dt.floor('s')
+        #     ### compute means
+        #     data = data.groupby(data['Time']).mean().reset_index()
+        #     ### compute results table for each column
+        #     for col in sub_cols[1:]:
+        #         c += 1
+        #         table = signal_df(data, col)
+        #         logger.info(f'Saving signal {c}/{len(cols)}: {col}')
+        #         ### save by date
+        #         for day in table.Date.unique():
+        #             ### save parquet files
+        #             table.loc[table.Date == day].to_parquet(path=args['out_path'], partition_on=['Date', 'Signal'], name_function=name_function)        
+    else:
+        logger.info(f'Historical data found!')
+        stored_data = dd.read_parquet(args['out_path'])
+        # stored_signals = list(stored_data.Signal.unique().compute())
+        
+        for col in cols:
+            if col in list(stored_data.Signal.unique().compute()):
+                logger.info(f'Signal {col} already in stored data')
+                stored_date = stored_data.loc[stored_data.Signal == col]['Time'].max().compute().floor("d",).strftime("%Y%m%d")
+                data = data.loc[(data['Time'] > stored_date)]
+                # stored_date = datetime.strptime(str(stored_date), '%Y%m%d')
+                # dd.read_parquet(args['out_path']).Signal.unique().compute()
+                print(stored_date)
+                # if start_date <= 
+            else:
+                logger.info(f'found new signal: {col}')
+                
+            
+
+                    
--- signal_functions.py
+++ signal_functions.py
@@ -1,4 +1,13 @@
+from dask import dataframe as dd
+
 def subranges(cols, num_sub_ranges, batch_size):
+    '''
+    Function to compute subranges based on batch size 
+    
+    Args:
+    
+    Return:
+    '''
     start_list = []
     end_list = []
     for i in range(0, num_sub_ranges):
@@ -11,14 +20,40 @@ def subranges(cols, num_sub_ranges, batch_size):
     return start_list, end_list
 
 def signal_df(df, signal):
-    ''' function to convert df format to output table format '''
-    signal_df = df.groupby(df["Time"],)[signal].mean().reset_index()
+    ''' 
+    Function to convert df format to output table format 
+    
+    Args:
+    
+    Return:
+    
+    '''
+    # signal_df = df.groupby(df["Time"],)[signal].mean().reset_index()
+    signal_df = df.dropna()
     signal_df["Date"] = df["Time"].dt.floor("d",).dt.strftime("%Y%m%d")
     signal_df["Signal"] = signal
     names = ["Time", "Value", "Date", "Signal"]
     return signal_df.rename(columns=dict(zip(signal_df.columns, names)))
 
+def check_history(path):
+    try:
+        dd.read_parquet(path)
+    except:
+        return True
+    else:
+        return False
 
+def process_signal(signal, start_date, path):
+    ### define subrange of columns
+    sub_cols = ['Time', signal]
+    ### read parquet
+    data = dd.read_parquet(path, columns=sub_cols)   
+    ### filter by date and round to seconds
+    data = data.loc[(data['Time'] > start_date)]
+    data['Time'] = data['Time'].dt.floor('s')
+    data = data.groupby(data['Time']).mean().reset_index()
+    data = signal_df(data, signal)
+    return data
 # def name_function(part_ind):
 #     Date = table["Date"].iat[0]
 #     name = Date + "_" + format(part_ind, '06d') + ".parquet"
