a88a5517503880fb601574209a612088508dee2f
salvador <guzmandalvador@gmail.com>
2023-05-09 20:17:09 -0600

    modified program

--- gap_detector_app/main.py
+++ gap_detector_app/main.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
 import argparse
-from dask import dataframe as dd
-from datetime import datetime, timedelta
 import json
 import logging as lg
-import os
-import pandas as pd
 import sys
-import time
 
-from utils import GapsData, StoredData
+from utils import GapsData
+from utils import Functions as fun
 
 def main():
     gaps = GapsData(**args)
     gaps.start_date = gaps.search_historical_data()
-    
-    data = StoredData(gaps.input_data)
-    for entry in data.scantree():
-        print(entry)
-
-
-
-
-
+    n_files = 0
+    for entry in fun.scantree(gaps.data_path):
+        if fun.check_starting_dates(entry, gaps.start_date):
+            gaps.gaps_data = gaps.get_gap_data(entry)
+        n_files += 1
+    ### filter gaps found
+    gaps.gaps_data = gaps.gaps_data[gaps.gaps_data['gaps'] == True]
+    ### check if gaps where found
+    if gaps.gaps_data.shape[0].compute() > 0:
+        logger.info(f'Found {gaps.gaps_data.shape[0].compute()} gaps')
+        gaps.save_data_by_date()
+    else: 
+        logger.info(f'No gaps foudn in {n_files+1} files')
 
 if __name__ == '__main__':
     ### define logger
@@ -40,77 +40,3 @@ if __name__ == '__main__':
 
     main()
     ########################
-
-
-    # logger.info(f'Loading dataframe...')
-    # for attempt in range(max_attemps+1):
-    #     if attempt < max_attemps:
-    #         logger.info(f'Reading parquet data from path: {args["input"]}')
-    #         try:
-    #             ### create empty dataframe
-    #             data = dd.DataFrame.from_dict({'file': [], 'Time': [], 'gaps':[], 'init_gap':[], 'end_gap':[]}, npartitions=1)
-    #             data['gaps'] = data['gaps'].astype(dtype='object')
-    #             ### read individual parquet file 
-    #             for n, entry in enumerate(gaps.scantree(args["input"])):
-    #                 ### check starting date
-    #                 if fun.check_dates(entry, start_date):
-    #                     ### find gaps and store values in data
-    #                     logger.info(f'Searching for gaps in file: {entry.path}')
-    #                     data = dd.concat([data, fun.read_gaps_from_file(entry.path, args['time_gap'])]).drop_duplicates().reset_index(drop=True)
-    #                 else:
-    #                     logger.info(f'No new data in file: {entry.path}')
-    #             ### filter gaps found
-    #             data = data.loc[data['gaps'] == True]
-    #             ### check if gaps where found
-    #             if data.shape[0].compute() > 0:
-    #                 logger.info(f'Found {data.shape[0].compute()} gaps in {n+1} files')
-    #                 ### reducing data and spliting by date
-    #                 data = data[['file', 'init_gap', 'end_gap']]
-
-    #                 ### saving by day or by date and hour
-    #                 if args['store_by'] == 'hour':
-    #                     logger.info(f'Saving data by hour')
-    #                     floor_val = 'H'
-    #                 elif args['store_by'] == 'day':
-    #                     floor_val = 'd'
-    #                     logger.info(f'Saving data by day')
-    #                 ### spliting gaps by date
-    #                 for time in dd.to_datetime(data['init_gap']).dt.floor(floor_val).unique():
-    #                     t = datetime.strptime(str(time), '%Y-%m-%d %H:%M:%S')
-                        
-    #                     logger.info(f'Saving gaps data from {t}')
-    #                     json_out = data.mask(dd.to_datetime(data['init_gap']).dt.floor(floor_val) != time).dropna()   
-    #                     json_out['init_gap'] = json_out['init_gap'].astype(str)
-    #                     json_out['end_gap'] = json_out['end_gap'].astype(str)
-
-    #                     def name_function(p_index):
-    #                         filename = (f'{t.strftime("%Y%m%d")}_{t.strftime("%H%M%S")}.json') 
-    #                         # filename = (f'{t.strftime("%Y%m%d")}_{p_index}.json')
-    #                         return filename
-                        
-    #                     ### DOES NOT WORK! using dask
-    #                     # dd.to_json(json_out, url_path=f'{args["out_path"]}/date={t.strftime("%Y%m%d")}/',
-    #                     #            name_function=name_function , orient='records') 
-
-    #                     ## IT WORKS! using pandas and creating folders separatedly
-    #                     json_out = json_out.compute()
-    #                     outdir = f'{args["out_path"]}/date={t.strftime("%Y%m%d")}/'
-    #                     if not os.path.exists(outdir):
-    #                         logger.info(f'Creating new folder {outdir}')
-    #                         os.makedirs(outdir)
-    #                     json_out.to_json(path_or_buf=f'{args["out_path"]}/date={t.strftime("%Y%m%d")}/{name_function(0)}',
-    #                                      orient='records', indent=4)
-    #                     logger.info(f'Gaps data saved into file: {args["out_path"]}/date={t.strftime("%Y%m%d")}/{name_function(0)}')
-    #             else:
-    #                 logger.info(f'No gaps where found')                
-    #         except:
-    #             logger.warning(f'Attempt number {attempt+1} failed. retry in {sleep_time} sec...')
-    #             time.sleep(sleep_time)
-    #         else:
-    #             logger.info(f'Parquet data analized succesfully')
-    #             break
-    #     else:
-    #         logger.error(f'ERROR: cannot read data...')
-
-
-
--- gap_detector_app/utils.py
+++ gap_detector_app/utils.py
@@ -1,14 +1,19 @@
 
 from dask import dataframe as dd
+from datetime import datetime, timedelta
 import logging as lg
-import pandas as pd
+import os
 from os import scandir
+import pandas as pd
+import time
+
 
 logger = lg.getLogger(__name__)
 class GapsData:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-
+        self.gaps_data = dd.DataFrame.from_dict({'file': [], 'Time': [], 'gaps':[], 'init_gap':[], 'end_gap':[]}, npartitions=1)
+    
     def search_historical_data(self):
         '''
         
@@ -29,26 +34,124 @@ class GapsData:
             start_date = start_date['init_gap'].tail(n=1).values[0]
             logger.info(f'Adding data created after {start_date}...')
             return start_date
-    
-class StoredData():
-    def __init__(self, data_path ):
-        self.data_path = data_path 
+        
+    def get_gap_data(self, entry):
+        logger.info(f'Searching for gaps in file: {entry.path}')
+        for attempt in range(self.max_reads_attemps+1):
+            if attempt < self.max_reads_attemps:        
+                try:
+                    data = dd.concat([self.gaps_data,
+                                      Functions.read_gaps_from_file(entry.path,
+                                                                    self.minimal_time_gap)]).drop_duplicates().reset_index(drop=True)
+                    return data
+                except:
+                    logger.warning(f'Cannot read file: {entry.path}')
+                    if attempt < self.max_reads_attemps:
+                        logger.info(f'retry in {self.retry_sleep_time}')
+                        time.sleep(self.retry_sleep_time)
+
+            else:
+                logger.error(f'cannot read file {entry.path}')
+        
+    def save_data_by_date(self):
+        ### reducing data and spliting by date
+        self.gaps_data = self.gaps_data[['file', 'init_gap', 'end_gap']]
+        ### saving by day or by date and hour
+        if self.split_data_stored_by == 'hour':
+            logger.info(f'Saving data by hour')
+            floor_val = 'H'
+        else:
+            logger.info(f'Saving data by day')
+            floor_val = 'd'
+        ### spliting gaps by date
+        for time in dd.to_datetime(self.gaps_data['init_gap']).dt.floor(floor_val).unique():
+            t = datetime.strptime(str(time), '%Y-%m-%d %H:%M:%S')
+            logger.info(f'Saving gaps data from {t}')
+            json_out = self.gaps_data.mask(dd.to_datetime(self.gaps_data['init_gap']).dt.floor(floor_val) != time).dropna()   
+            json_out['init_gap'] = json_out['init_gap'].astype(str)
+            json_out['end_gap'] = json_out['end_gap'].astype(str)
+            self.save_json(json_out, t)
 
 
 
-    def scantree(self):
+                
+
+    def save_json(self, json_out, t):
+        def name_function(p_index):
+            filename = (f'{t.strftime("%Y%m%d")}_{t.strftime("%H%M%S")}.json') 
+            # filename = (f'{t.strftime("%Y%m%d")}_{p_index}.json')
+            return filename
+        logger.info(f'Writing gaps data into file: {self.output_data}/date={t.strftime("%Y%m%d")}/{name_function(0)}')
+        for attempt in range(self.max_reads_attemps+1):
+            if attempt < self.max_reads_attemps:        
+                try:
+                    ### DOES NOT WORK! using dask
+                    # dd.to_json(json_out, url_path=f'{args["out_path"]}/date={t.strftime("%Y%m%d")}/',
+                    #            name_function=name_function , orient='records') 
+                    ### IT WORKS! using pandas and creating folders separatedly
+
+                    json_out = json_out.compute()
+                    outdir = f'{self.output_data}/date={t.strftime("%Y%m%d")}/'
+                    if not os.path.exists(outdir):
+                        logger.info(f'Creating new folder {outdir}')
+                        os.makedirs(outdir)
+                    json_out.to_json(path_or_buf=f'{self.output_data}/date={t.strftime("%Y%m%d")}/{name_function(0)}',
+                                        orient='records', indent=4)
+                    logger.info(f'Gaps data saved into file: {self.output_data}/date={t.strftime("%Y%m%d")}/{name_function(0)}')
+                except:
+                    logger.warning(f'Cannot write file: ')
+                    if attempt < self.max_reads_attemps:
+                        logger.info(f'retry in {self.retry_sleep_time}')
+                        time.sleep(self.retry_sleep_time)
+                else:
+                    break
+            else: 
+                logger.error(f'cannot write file')
+
+               
+class Functions:
+    def scantree(path):
         ''''
         Recursively yield DirEntry objects for given directory.
         Taken from: https://stackoverflow.com/a/33135143        
         '''
-        for entry in scandir(self.data_path):
+        for entry in scandir(path):
             if entry.is_dir(follow_symlinks=False):
-                yield from functions.scantree(entry.path)  
+                yield from Functions.scantree(entry.path)  
             else:
                 yield entry
-        
 
-class functions:
+    def check_starting_dates(path, start_date):
+            data = dd.read_parquet(path, columns='Time')
+            data = data.mask(data < start_date).dropna()
+            if data.shape[0].compute() > 0:
+                logger.info(f'Found recent data in file {path}')
+                return True
+            else:
+                logger.info(f'No new data found in file {path}')
+                return False
+
+    def read_gaps_from_file(path, time_gap):
+        '''
+            identify time gaps in  parquet file
+            
+            Args: 
+                path: path to parquet file
+            
+            Return:
+                A dask data frame with three columns: [file, Time, gaps]
+        '''
+        df = dd.DataFrame.from_dict({'file': [], 'Time': [], 'gaps':[], 'init_gap':[], 'end_gap':[]}, npartitions=1)
+        df['Time'] = dd.read_parquet(path, columns='Time').dt.floor('s').unique()
+        df = df.sort_values(by="Time")
+        df['gaps'] = df['Time'].diff() > pd.to_timedelta(time_gap)
+        df['gaps'] = df['gaps'].astype(dtype='object')
+        df['end_gap'] = df['Time'].mask(df['gaps'] != True)
+        df['init_gap'] = df['Time'].shift(1).mask(df['gaps'] != True)
+        df['file'] = path
+        return(df)
+
+
     def check_history(path):
         '''
         
@@ -77,36 +180,8 @@ class functions:
 
 
 
-    def read_gaps_from_file(path, time_gap):
-        '''
-            identify time gaps in  parquet file
-            
-            Args: 
-                path: path to parquet file
-            
-            Return:
-                A dask data frame with three columns: [file, Time, gaps]
-        '''
-        df = dd.DataFrame.from_dict({'file': [], 'Time': [], 'gaps':[], 'init_gap':[], 'end_gap':[]}, npartitions=1)
-        df['Time'] = dd.read_parquet(path, columns='Time').dt.floor('s').unique()
-        df = df.sort_values(by="Time")
-        df['gaps'] = df['Time'].diff() > pd.to_timedelta(time_gap)
-        df['gaps'] = df['gaps'].astype(dtype='object')
-        df['end_gap'] = df['Time'].mask(df['gaps'] != True)
-        df['init_gap'] = df['Time'].shift(1).mask(df['gaps'] != True)
-        df['file'] = path
-        return(df)
 
-    def check_dates(path, start_date):
-        if start_date != None:
-            data = dd.read_parquet(path, columns='Time')
-            data = data.mask(data < start_date).dropna()
-            if data.shape[0].compute() > 0:
-                return True
-            else:
-                return False
-        else:
-            return True
+
 class aditional_functions:     
     def signal_df(df, signal):
         ''' 
